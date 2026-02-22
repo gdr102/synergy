dedce7f977e0dee807d1a26830972b7583edd2b1
gdr102 <gdr102@ya.ru>
2026-02-22 16:36:26 +1000

    add new files lesson 7

---
+++ lesson 7/task1.py
@@ -0,0 +1,8 @@
+s = input('Введите слово: ')
+
+s_revers = s[::-1]
+
+if s == s_revers:
+    print('yes')
+else:
+    print('no')
---
+++ lesson 7/task2.py
@@ -0,0 +1,7 @@
+text = input('Введите строку (до 1000 символов): ')
+
+words = text.split()
+
+result = ' '.join(words)
+
+print('Результат:', result)
